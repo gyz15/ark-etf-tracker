5bc8d6a7ab7379d4f97913490aef8e6112f2aa2d
Gnaw Yuan Zhen <yzgnaw@gmail.com>
2021-01-04 21:34:00 +0800

    Import is_valid_action_request

--- bot/views.py
+++ bot/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 from decouple import config
-from .ark import find_ark
+from .ark import find_ark, is_valid_action_request
 
 # Create your views here.
 def main(request):
     return JsonResponse({'ok':'Nothing happened actually'})
 
-    
+
 def update_ark(request):
     if is_valid_action_request(request):
         find_ark()
